58f31c0110e28f4dce4373adb5932688f7ca9823
Chema Balsas <jose.balsas@liferay.com>
2015-05-05 16:04:26 +0200

    LPS-55175 Simplify ItemViewer module

--- modules/frontend/frontend-js-web/src/META-INF/resources/html/js/liferay/item_viewer.js
+++ modules/frontend/frontend-js-web/src/META-INF/resources/html/js/liferay/item_viewer.js
@@ -9,7 +9,11 @@ AUI.add(
 
 		var CSS_FOOTER_CONTROL_LEFT = A.getClassName('image', 'viewer', 'footer', 'control', 'left');
 
-		var CSS_FOOTER_CONTROL_RIGHT = A.getClassName('image', 'viewer', 'footer', 'control', 'rigth');
+		var CSS_FOOTER_CONTROL_LEFT_BASE = A.getClassName('image', 'viewer', 'base', 'control', 'left');
+
+		var CSS_FOOTER_CONTROL_RIGHT = A.getClassName('image', 'viewer', 'footer', 'control', 'right');
+
+		var CSS_FOOTER_CONTROL_RIGHT_BASE = A.getClassName('image', 'viewer', 'base', 'control', 'right');
 
 		var CSS_IMAGE_CONTAINER = A.getClassName('image', 'viewer', 'base', 'image', 'container');
 
@@ -19,20 +23,9 @@ AUI.add(
 
 		var CSS_LOADING_ICON = A.getClassName('image', 'viewer', 'base', 'loading', 'icon');
 
-		var EVENT_CURRENT_IMAGE = 'itemSelectorDialog:currentImage';
-
-		var EVENT_TOGGLE_BUTTON = 'itemSelectorDialog:toggleButton';
+		var STR_RENDER_CONTROLS = 'renderControls';
 
 		var TPL_CLOSE = '<button class="close image-viewer-base-control image-viewer-close lfr-item-viewer" type="button"><span class="glyphicon glyphicon-chevron-left"></span><h4>{0}</h4></button>';
-
-		var TPL_CONTROL_LEFT = '<a href="#" class="' + CSS_FOOTER_CONTROL + ' ' + CSS_FOOTER_CONTROL_LEFT + '"><span class="glyphicon glyphicon-chevron-left"></span></a>';
-
-		var TPL_CONTROL_RIGHT = '<a href="#" class="' + CSS_FOOTER_CONTROL + ' ' + CSS_FOOTER_CONTROL_RIGHT + '"><span class="glyphicon glyphicon-chevron-right"></span></a>';
-
-		var TPL_FOOTER_BUTTONS = '<div class="' + CSS_FOOTER_BUTTONS + '"><span class="glyphicon glyphicon-info-sign"></span></div>';
-
-		var TPL_IMAGE_CONTAINER = '<div class="' + CSS_IMAGE_CONTAINER + '"> <div class="' + CSS_IMAGE_INFO + '"></div>' +
-			'<span class="glyphicon glyphicon-time ' + CSS_LOADING_ICON + '"></span></div>';
 
 		var LiferayItemViewer = A.Component.create(
 			{
@@ -79,24 +72,20 @@ AUI.add(
 				NS: 'lfr-item-viewer',
 
 				prototype: {
+					TPL_CONTROL_LEFT: '<a href="#" class="' + CSS_FOOTER_CONTROL + ' ' + CSS_FOOTER_CONTROL_LEFT_BASE + ' ' + CSS_FOOTER_CONTROL_LEFT + '"><span class="glyphicon glyphicon-chevron-left"></span></a>',
+
+					TPL_CONTROL_RIGHT: '<a href="#" class="' + CSS_FOOTER_CONTROL + ' ' + CSS_FOOTER_CONTROL_RIGHT_BASE + ' ' + CSS_FOOTER_CONTROL_RIGHT + '"><span class="glyphicon glyphicon-chevron-right"></span></a>',
+
+					TPL_FOOTER_BUTTONS: '<div class="' + CSS_FOOTER_BUTTONS + '"><span class="glyphicon glyphicon-info-sign"></span></div>',
+
+					TPL_IMAGE_CONTAINER: '<div class="' + CSS_IMAGE_CONTAINER + '"> <div class="' + CSS_IMAGE_INFO + '"></div><span class="glyphicon glyphicon-time ' + CSS_LOADING_ICON + '"></span></div>',
+
 					initializer: function() {
 						var instance = this;
 
-						instance.TPL_IMAGE_CONTAINER = TPL_IMAGE_CONTAINER;
-					},
-
-					renderUI: function() {
-						var instance = this;
-
-						A.ImageViewer.superclass.renderUI.apply(instance, arguments);
-
-						instance._renderFooter();
-
-						instance._fire(
-							EVENT_TOGGLE_BUTTON,
-							{
-								disabled: true
-							}
+						instance.TPL_CLOSE = Lang.sub(
+							TPL_CLOSE,
+							[instance.get('btnCloseCaption')]
 						);
 					},
 
@@ -105,64 +94,24 @@ AUI.add(
 
 						LiferayItemViewer.superclass.bindUI.apply(instance, arguments);
 
-						instance._eventHandles = [
-							instance.footerNode.delegate('click', instance._onControlsClick, '.' + CSS_FOOTER_CONTROL, instance),
-							instance._footerButtons.delegate('click', instance._onInfoClick, '.glyphicon-info-sign', instance),
-							Liferay.after('showTab', instance._syncImageInfoUI, instance),
-							A.after(instance._afterUIVisible, instance, '_uiSetVisible')
-						];
-					},
-
-					destructor: function() {
-						var instance = this;
-
-						(new A.EventHandle(instance._eventHandles)).detach();
-					},
-
-					_afterUIVisible: function() {
-						var instance = this;
-
-						instance._fire(
-							EVENT_TOGGLE_BUTTON,
-							{
-								disabled: !instance.get('visible')
-							}
+						instance._eventHandles.push(
+							instance._footerButtons.delegate('click', instance._onInfoClick, '.' + CSS_FOOTER_BUTTONS, instance),
+							Liferay.after('showTab', instance._syncImageInfoUI, instance)
 						);
 					},
 
-					_fire: function(event, details) {
-						Liferay.Util.getOpener().Liferay.fire(event, details);
-					},
-
-					_onControlsClick: function(event) {
+					_onClickControl: function(event) {
 						var instance = this;
 
-						event.preventDefault();
 						event.stopImmediatePropagation();
 
-						if (event.currentTarget.hasClass(CSS_FOOTER_CONTROL_LEFT)) {
-							instance.prev();
-						}
-						else if (event.currentTarget.hasClass(CSS_FOOTER_CONTROL_RIGHT)) {
-							instance.next();
-						}
+						LiferayItemViewer.superclass._onClickControl.apply(instance, arguments);
 					},
 
 					_onInfoClick: function(event) {
 						var instance = this;
 
-						instance.get('contentBox').all('.' + CSS_IMAGE_CONTAINER + ' .' + CSS_IMAGE_INFO).toggleClass('show-info');
-					},
-
-					_renderControls: function() {
-						var instance = this;
-
-						var body = A.one('body');
-
-						var btnCloseTemplate = Lang.sub(TPL_CLOSE, [instance.get('btnCloseCaption')]);
-
-						instance._closeEl = A.Node.create(btnCloseTemplate);
-						body.append(instance._closeEl);
+						instance.get('contentBox').toggleClass('show-info');
 					},
 
 					_renderFooter: function() {
@@ -176,41 +125,28 @@ AUI.add(
 						instance._captionEl.selectable();
 						container.append(instance._captionEl);
 
-						var renderControls = instance.get('renderControls');
+						if (instance.get(STR_RENDER_CONTROLS)) {
+							container.append(instance.get('controlPrevious'));
 
-						if (renderControls) {
-							container.append(A.Node.create(TPL_CONTROL_LEFT));
+							instance._infoEl = A.Node.create(instance.TPL_INFO);
+							instance._infoEl.selectable();
+							container.append(instance._infoEl);
+
+							container.append(instance.get('controlNext'));
 						}
 
-						instance._infoEl = A.Node.create(instance.TPL_INFO);
-						instance._infoEl.selectable();
-						container.append(instance._infoEl);
-
-						if (renderControls) {
-							container.append(A.Node.create(TPL_CONTROL_RIGHT));
-						}
-
-						instance._footerButtons = A.Node.create(TPL_FOOTER_BUTTONS);
+						instance._footerButtons = A.Node.create(instance.TPL_FOOTER_BUTTONS);
 						container.append(instance._footerButtons);
 					},
 
-					_showCurrentImage: function() {
+					_syncInfoUI: function() {
 						var instance = this;
 
-						A.ImageViewer.superclass._showCurrentImage.apply(instance, arguments);
+						if (instance.get(STR_RENDER_CONTROLS)) {
+							LiferayItemViewer.superclass._syncInfoUI.apply(instance, arguments);
+						}
 
-						instance._syncCaptionUI();
-						instance._syncInfoUI();
 						instance._syncImageInfoUI();
-
-						var link = instance.get('links').item(instance.get('currentIndex'));
-
-						instance._fire(
-							EVENT_CURRENT_IMAGE,
-							{
-								url: link.getData('url') || link.getAttribute('href')
-							}
-						);
 					},
 
 					_syncImageInfoUI: function(event) {
@@ -218,12 +154,14 @@ AUI.add(
 
 						var link = instance.get('links').item(instance.get('currentIndex'));
 
-						var infoHTML = link.siblings('.image-info') ? link.siblings('.image-info').html() : '';
+						if (link) {
+							var infoHTML = link.siblings('.image-info') ? link.siblings('.image-info').html() : '';
 
-						var imageInfoNode = instance.get('contentBox').one('.' + CSS_IMAGE_CURRENT_CONTAINER + ' .' + CSS_IMAGE_INFO);
+							var imageInfoNode = instance.get('contentBox').one('.' + CSS_IMAGE_CURRENT_CONTAINER + ' .' + CSS_IMAGE_INFO);
 
-						if (imageInfoNode && infoHTML) {
-							imageInfoNode.html(infoHTML);
+							if (imageInfoNode && infoHTML) {
+								imageInfoNode.html(infoHTML);
+							}
 						}
 					}
 				}
