ac8a90202d72e1700475e091efcf19437ad11661
Roberto Díaz <roberto.diaz@liferay.com>
2015-06-10 17:19:35 +0200

    LPS-56201 Add method to could reuse the base with more jsps

--- modules/apps/document-library/document-library-item-selector-web/src/com/liferay/document/library/item/selector/web/image/DLImageDocumentItemSelectorView.java
+++ modules/apps/document-library/document-library-item-selector-web/src/com/liferay/document/library/item/selector/web/image/DLImageDocumentItemSelectorView.java
@@ -12,12 +12,13 @@
  * details.
  */
 
-package com.liferay.document.library.item.selector.web.file;
+package com.liferay.document.library.item.selector.web.image;
 
 import com.liferay.document.library.item.selector.web.BaseDLItemSelectorView;
 import com.liferay.item.selector.ItemSelectorView;
 import com.liferay.item.selector.criteria.DefaultItemSelectorReturnType;
-import com.liferay.item.selector.criteria.file.criterion.FileItemSelectorCriterion;
+import com.liferay.item.selector.criteria.image.criterion.ImageItemSelectorCriterion;
+import com.liferay.portal.util.PropsValues;
 
 import java.util.HashSet;
 import java.util.Locale;
@@ -30,13 +31,23 @@ import org.osgi.service.component.annotations.Component;
  * @author Roberto Díaz
  */
 @Component(service = ItemSelectorView.class)
-public class DLFileItemSelectorView
+public class DLImageDocumentItemSelectorView
 	extends BaseDLItemSelectorView
-		<FileItemSelectorCriterion, DefaultItemSelectorReturnType> {
+		<ImageItemSelectorCriterion, DefaultItemSelectorReturnType> {
 
 	@Override
-	public Class<FileItemSelectorCriterion> getItemSelectorCriterionClass() {
-		return FileItemSelectorCriterion.class;
+	public Class<ImageItemSelectorCriterion> getItemSelectorCriterionClass() {
+		return ImageItemSelectorCriterion.class;
+	}
+
+	@Override
+	public String getJspPath() {
+		return "/o/document-library-item-selector-web/documents.jsp";
+	}
+
+	@Override
+	public String[] getMimeTypes() {
+		return PropsValues.DL_FILE_ENTRY_PREVIEW_IMAGE_MIME_TYPES;
 	}
 
 	@Override
