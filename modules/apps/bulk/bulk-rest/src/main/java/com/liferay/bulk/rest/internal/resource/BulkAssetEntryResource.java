9779ac834cd2658cc08e0b20683f3296939cf142
Alejandro Tardín <alejandro.tardin@liferay.com>
2019-02-11 18:44:46 +0100

    LPS-90340 Adding vocabularies to the response

--- modules/apps/bulk/bulk-rest/src/main/java/com/liferay/bulk/rest/internal/resource/BulkAssetEntryResource.java
+++ modules/apps/bulk/bulk-rest/src/main/java/com/liferay/bulk/rest/internal/resource/BulkAssetEntryResource.java
@@ -16,8 +16,10 @@ package com.liferay.bulk.rest.internal.resource;
 
 import com.liferay.asset.kernel.model.AssetCategory;
 import com.liferay.asset.kernel.model.AssetEntry;
+import com.liferay.asset.kernel.model.AssetVocabulary;
 import com.liferay.asset.kernel.service.AssetCategoryLocalService;
 import com.liferay.asset.kernel.service.AssetTagLocalService;
+import com.liferay.asset.kernel.service.AssetVocabularyLocalService;
 import com.liferay.bulk.rest.internal.model.BulkActionResponseModel;
 import com.liferay.bulk.rest.internal.model.BulkAssetEntryActionModel;
 import com.liferay.bulk.rest.internal.model.BulkAssetEntryCommonCategoriesModel;
@@ -45,6 +47,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
@@ -99,19 +102,19 @@ public class BulkAssetEntryResource {
 
 			Stream<AssetEntry> stream = assetEntryBulkSelection.stream();
 
-			Stream<AssetCategory> commonCategoriesStream = stream.map(
+			Set<AssetCategory> commonCategories = stream.map(
 				_getAssetEntryCategoriesFunction(
 					PermissionCheckerFactoryUtil.create(user))
 			).reduce(
 				SetUtil::intersect
 			).orElse(
 				Collections.emptySet()
-			).stream();
+			);
 
 			return new BulkAssetEntryCommonCategoriesModel(
 				bulkSelection.describe(locale),
-				commonCategoriesStream.collect(
-					Collectors.groupingBy(AssetCategory::getVocabularyId)));
+				_groupByAssetVocabulary(commonCategories),
+				_getAssetVocabularies(commonCategories));
 		}
 		catch (Exception e) {
 			return new BulkAssetEntryCommonCategoriesModel(e);
@@ -286,11 +289,38 @@ public class BulkAssetEntryResource {
 		};
 	}
 
+	private List<AssetVocabulary> _getAssetVocabularies(
+		Set<AssetCategory> assetCategories) {
+
+		Stream<AssetCategory> assetCategoryStream = assetCategories.stream();
+
+		return assetCategoryStream.map(
+			AssetCategory::getVocabularyId
+		).distinct(
+		).map(
+			_assetVocabularyLocalService::fetchAssetVocabulary
+		).collect(
+			Collectors.toList()
+		);
+	}
+
+	private Map<Long, List<AssetCategory>> _groupByAssetVocabulary(
+		Set<AssetCategory> assetCategories) {
+
+		Stream<AssetCategory> assetCategoryStream = assetCategories.stream();
+
+		return assetCategoryStream.collect(
+			Collectors.groupingBy(AssetCategory::getVocabularyId));
+	}
+
 	@Reference
 	private AssetCategoryLocalService _assetCategoryLocalService;
 
 	@Reference
 	private AssetTagLocalService _assetTagLocalService;
+
+	@Reference
+	private AssetVocabularyLocalService _assetVocabularyLocalService;
 
 	@Reference
 	private BulkSelectionFactoryRegistry _bulkSelectionFactoryRegistry;
