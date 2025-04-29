356f4f83c37a659ed1696e44de3efaf674dc0191
Jürgen Kappler <jurgen.kappler@liferay.com>
2025-04-29 11:37:21 +0200

    LPD-54027 Read values from the request and set them accordingly

--- modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/fragment/renderer/EditFolderFragmentRenderer.java
+++ modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/fragment/renderer/EditFolderFragmentRenderer.java
@@ -8,6 +8,8 @@ package com.liferay.site.cms.site.initializer.internal.fragment.renderer;
 import com.liferay.fragment.renderer.FragmentRenderer;
 import com.liferay.fragment.renderer.FragmentRendererContext;
 import com.liferay.frontend.taglib.react.servlet.taglib.ComponentTag;
+import com.liferay.info.constants.InfoDisplayWebKeys;
+import com.liferay.object.model.ObjectEntryFolder;
 import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.ParamUtil;
@@ -65,9 +67,22 @@ public class EditFolderFragmentRenderer extends BaseSectionFragmentRenderer {
 			componentTag.setProps(
 				HashMapBuilder.<String, Object>put(
 					"backURL",
-					ParamUtil.getString(httpServletRequest, "backURL") //TODO get current url
+					ParamUtil.getString(httpServletRequest, "redirect")
 				).put(
-					"folderId", "description" //TODO get folderId from parameters
+					"folderId",
+					() -> {
+						Object object = httpServletRequest.getAttribute(
+							InfoDisplayWebKeys.INFO_ITEM);
+
+						if (object instanceof ObjectEntryFolder) {
+							ObjectEntryFolder objectEntryFolder =
+								(ObjectEntryFolder)object;
+
+							return objectEntryFolder.getObjectEntryFolderId();
+						}
+
+						return null;
+					}
 				).build());
 
 			componentTag.setServletContext(_servletContext);
